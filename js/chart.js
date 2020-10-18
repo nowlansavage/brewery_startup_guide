6bf80cc7af2c9154f2424bd0345523165c2ff94b
nowlansavage <nowlansavage@gmail.com>
2020-10-18 17:51:25 -0500

    Updated chart

--- js/chart.js
+++ js/chart.js
@@ -4,6 +4,7 @@ d3.json("http://localhost:5000/Beer_Advocate").then(function(data){
 
 d3.json("http://localhost:5000/Brewers_Association").then(function(data){
     console.log(data);
+    dropDownMap(data);
 });
 
 d3.json("http://localhost:5000/City_Pop_Brew_Count").then(function(data){
@@ -12,4 +13,23 @@ d3.json("http://localhost:5000/City_Pop_Brew_Count").then(function(data){
 
 d3.json("http://localhost:5000/Rate_Beer").then(function(data){
     console.log(data);
+    dropDownBeer(data);
 });
+
+function dropDownMap(sampleData){
+	sampleData.forEach(name=>{
+		var states = name['state']
+		var newItem = d3.select('#selDatasetMap').append('option');
+		newItem.text(states);
+		newItem.property('value', states)
+	});
+};
+
+function dropDownBeer(sampleData){
+	sampleData.forEach(name=>{
+		var beer= name['Style']
+		var newItem = d3.select('#selDatasetBeer').append('option');
+		newItem.text(beer);
+		newItem.property('value', beer)
+	});
+};
