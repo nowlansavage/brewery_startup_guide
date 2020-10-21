a4c638d41780bfc6ffe11173d184533e9d0b7eeb
nowlansavage <nowlansavage@gmail.com>
2020-10-21 10:50:48 -0500

    chart made

--- js/chart.js
+++ js/chart.js
@@ -1,16 +1,9 @@
-d3.json("http://localhost:5000/Beer_Advocate").then(function(data){
-    console.log(data);
-});
 
 d3.json("http://localhost:5000/Brewers_Association").then(function(data){
     console.log(data);
     dropDownMap(data);
    });
 
-d3.json("http://localhost:5000/City_Pop_Brew_Count").then(function(data){
-    console.log(data);
-
-});
 
 d3.json("http://localhost:5000/Rate_Beer").then(function(data){
     console.log(data);
@@ -123,6 +116,7 @@ function makeChart_first(){
 		};
 		var data =[traceBeer];
 		var layout ={
+			title: {text: 'Beer Rating vs ABV'},
 			xaxis: { title: "ABV" },
 			yaxis: { title: 'Beer Rating'}
 			
@@ -136,21 +130,119 @@ makeChart_first();
 //distribution of abv beers
 function makeChart_distribution(){
 	d3.json("http://localhost:5000/Rate_Beer").then(function(data){
+		var filter10=data.filter(x=>(x['ABV']*100)>=10).sort((a,b)=> (a.ABV > b.ABV) ? 1 : -1).sort((a,b)=> (a.SCORE > b.SCORE) ? 1 : -1);
+		var x10 = filter10.map(x=>x['SCORE']);
+		var y10 = x10.map((x,index)=>(index+1)/x10.length);
+		//9-10 ABV
+		var filter9=data.filter(x=>(x['ABV']*100)>=9 && (x['ABV']*100)<10).sort((a,b)=> (a.ABV > b.ABV) ? 1 : -1).sort((a,b)=> (a.SCORE > b.SCORE) ? 1 : -1);
+		var x9 = filter9.map(x=>x['SCORE']);
+		var y9 = x9.map((x,index)=>(index+1)/x9.length);
+		//8-9 ABV
+		var filter8=data.filter(x=>(x['ABV']*100)>=8 && (x['ABV']*100)<9).sort((a,b)=> (a.ABV > b.ABV) ? 1 : -1).sort((a,b)=> (a.SCORE > b.SCORE) ? 1 : -1);
+		var x8 = filter8.map(x=>x['SCORE']);
+		var y8 = x8.map((x,index)=>(index+1)/x8.length);
+		// 7-8 ABV
+		var filter7=data.filter(x=>(x['ABV']*100)>=7 && (x['ABV']*100)<8).sort((a,b)=> (a.ABV > b.ABV) ? 1 : -1).sort((a,b)=> (a.SCORE > b.SCORE) ? 1 : -1);
+		var x7 = filter7.map(x=>x['SCORE']);
+		var y7 = x7.map((x,index)=>(index+1)/x7.length);
+		//6-7 ABV
+		var filter6=data.filter(x=>(x['ABV']*100)>=6 && (x['ABV']*100)<7).sort((a,b)=> (a.ABV > b.ABV) ? 1 : -1).sort((a,b)=> (a.SCORE > b.SCORE) ? 1 : -1);
+		var x6 = filter6.map(x=>x['SCORE']);
+		var y6 = x6.map((x,index)=>(index+1)/x6.length);
+		//5-6 ABV
+		var filter5=data.filter(x=>(x['ABV']*100)>=5 && (x['ABV']*100)<6).sort((a,b)=> (a.ABV > b.ABV) ? 1 : -1).sort((a,b)=> (a.SCORE > b.SCORE) ? 1 : -1);
+		var x5 = filter5.map(x=>x['SCORE']);
+		var y5 = x5.map((x,index)=>(index+1)/x5.length);
+		//4-5 ABV
+		var filter4=data.filter(x=>(x['ABV']*100)>=4 && (x['ABV']*100)<5).sort((a,b)=> (a.ABV > b.ABV) ? 1 : -1).sort((a,b)=> (a.SCORE > b.SCORE) ? 1 : -1);
+		var x4 = filter4.map(x=>x['SCORE']);
+		var y4 = x4.map((x,index)=>(index+1)/x4.length);
+		//3-4
+		var filter3=data.filter(x=>(x['ABV']*100)>=3 && (x['ABV']*100)<4).sort((a,b)=> (a.ABV > b.ABV) ? 1 : -1).sort((a,b)=> (a.SCORE > b.SCORE) ? 1 : -1);
+		var x3 = filter3.map(x=>x['SCORE']);
+		var y3 = x3.map((x,index)=>(index+1)/x3.length);
+		//under 3
+		var filter2=data.filter(x=>(x['ABV']*100)<3).sort((a,b)=> (a.ABV > b.ABV) ? 1 : -1).sort((a,b)=> (a.SCORE > b.SCORE) ? 1 : -1);
+		var x2 = filter2.map(x=>x['SCORE']);
+		var y2 = x2.map((x,index)=>(index+1)/x2.length);
+
 		//chart map data
-		var trace1={
-			x: new ten = data.map(x=>x['ABV'] >=10),
-			y: data.map(y=>y['SCORE']),
-			text: data.map(x=>x['NAME']),
+		var ABV10={
+			x: x10,
+			y: y10,
 			mode: 'markers',
-			type: 'scatter'
+			type: 'scatter',
+			name: 'ABV 10%+'
 		};
-		var data =[trace1];
+
+		var ABV9={
+			x: x9,
+			y: y9,
+			mode: 'markers',
+			type: 'scatter',
+			name: 'ABV 9-10%'
+		};
+		var ABV8={
+			x: x8,
+			y: y8,
+			mode: 'markers',
+			type: 'scatter',
+			name: 'ABV 8-9%'
+		};
+
+		var ABV7={
+			x: x7,
+			y: y7,
+			mode: 'markers',
+			type: 'scatter',
+			name: 'ABV 7-8%'
+		};
+
+		var ABV6={
+			x: x6,
+			y: y6,
+			mode: 'markers',
+			type: 'scatter',
+			name: 'ABV 6-7%'
+		};
+		var ABV5={
+			x: x5,
+			y: y5,
+			mode: 'markers',
+			type: 'scatter',
+			name: 'ABV 5-6%'
+		};
+		var ABV4={
+			x: x4,
+			y: y4,
+			mode: 'markers',
+			type: 'scatter',
+			name: 'ABV 4-5%'
+		};
+		var ABV3={
+			x: x3,
+			y: y3,
+			mode: 'markers',
+			type: 'scatter',
+			name: 'ABV 3-4%'
+		};
+		var ABV2={
+			x: x2,
+			y: y2,
+			mode: 'markers',
+			type: 'scatter',
+			name: 'ABV <3%'
+		};
+		var data =[ABV10, ABV9, ABV8, ABV7, ABV6, ABV5, ABV4, ABV3, ABV2];
 		var layout ={
-			xaxis: { title: "ABV" },
-			yaxis: { title: 'Beer Rating'}
+			title: { text: "Distribution of Beer Ratings as a Function of ABV"},
+			xaxis: { title: "Rating" },
+			yaxis: { title: 'Distribution'}
 			
 		};
 		Plotly.newPlot('abv-chart-distribute', data, layout);
 	});
 };
+
+makeChart_distribution();
 
